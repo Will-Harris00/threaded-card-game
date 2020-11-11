c57f06c683a266959a233d51acfe4bb93a61e721
Isaac Cheng <isaaccheng9@gmail.com>
2020-11-11 16:08:56 +0000

    Created test file for Player.java

---
+++ PlayerTest.java
@@ -0,0 +1,82 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getHand() {
+    }
+
+    @Test
+    void getHandCard() {
+    }
+
+    @Test
+    void getHandSize() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void addToHand() {
+    }
+
+    @Test
+    void remFromHand() {
+    }
+
+    @Test
+    void drawValue() {
+    }
+
+    @Test
+    void draw() {
+    }
+
+    @Test
+    void discard() {
+    }
+
+    @Test
+    void keep() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void viewArray() {
+    }
+
+    @Test
+    void strategy() {
+    }
+
+    @Test
+    void writeToFile() {
+    }
+
+    @Test
+    void createFile() {
+    }
+
+    @Test
+    void run() {
+    }
+
+    @Test
+    void isWinner() {
+    }
+}
