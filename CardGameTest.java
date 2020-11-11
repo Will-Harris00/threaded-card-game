bf16d4d2df8621af7290aa47ad8bada2b9498945
Isaac Cheng <isaaccheng9@gmail.com>
2020-11-11 20:45:44 +0000

    Added tests for validateNumPlayersInput

--- CardGameTest.java
+++ CardGameTest.java
@@ -2,22 +2,77 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
 public class CardGameTest {
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
     }
 
     @Test
     public void main() {
     }
 
+    /**
+     * Tests validateNumPlayersInput method with a valid input of a positive integer.
+     *
+     * @throws NumberFormatException When an input which cannot be parsed as an integer is entered.
+     */
     @Test
-    public void validateNumPlayersInput() {
+    public void testValidateNumPlayersInput() throws NumberFormatException, IllegalNumPlayersSizeException {
+        // Creates an input which can be parsed as an integer greater than 1.
+        Scanner inputPlayersPosiInt = new Scanner("4");
+        int expected = 4;
+        int result = CardGame.validateNumPlayersInput(inputPlayersPosiInt);
+        assertEquals(expected, result);
+    }
+
+    // Tests validateNumPlayersInput with an invalid input of an integer which is too small.
+    @Test
+    public void testValidateNumPlayersInput_IllegalSmallValue() {
+        // Creates an input which can be parsed as an integer, but is too small.
+        try {
+            Scanner inputPlayersString = new Scanner("1");
+            CardGame.validateNumPlayersInput(inputPlayersString);
+            fail("Should have thrown IllegalNumPlayersSizeException.");
+        } catch (IllegalNumPlayersSizeException e) {
+            assertTrue(true);
+        }
+    }
+
+    // Tests validateNumPlayersInput with an invalid input of a negative integer value.
+    @Test
+    public void testValidateNumPlayersInput_IllegalNegValue() {
+        // Creates an input which can be parsed as an integer, but is negative.
+        try {
+            Scanner inputPlayersString = new Scanner("-1");
+            CardGame.validateNumPlayersInput(inputPlayersString);
+            fail("Should have thrown IllegalNumPlayersSizeException.");
+        } catch (IllegalNumPlayersSizeException e) {
+            assertTrue(true);
+        }
+    }
+
+    /**
+     * Tests validateNumPlayersInput method with an invalid input of a string.
+     *
+     * @throws NumberFormatException          When an input which cannot be parsed as an integer is entered.
+     * @throws IllegalNumPlayersSizeException When an input which can be parsed as an integer, but is too small, is
+     *                                        entered.
+     */
+    @Test(expected = NumberFormatException.class)
+    public void testValidateNumPlayersInput_IllegalStringValue() throws NumberFormatException, IllegalNumPlayersSizeException {
+        // Creates an input which cannot be passed as an integer (only a string).
+        Scanner inputPlayersString = new Scanner("string");
+        CardGame.validateNumPlayersInput(inputPlayersString);
+        fail("NumberFormatException should have been thrown.");
     }
 
     @Test
